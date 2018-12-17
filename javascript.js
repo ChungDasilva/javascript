12b30ce70da795b0c8543fc98f333e0b3647cfb3
Nguyen Ha Chung <chung12dt@gmail.com>
2018-12-17 12:08:46 +0900

    csc

---
+++ javascript.js
@@ -0,0 +1,87 @@
+
+//callback
+// Execute hàm doThis với một function  andThenThis là param. doThis sẽ thực thi bất kỳ đoạn code nào bên trong nó và khi kết thúc andThenThis sẽ được thực thi
+doThis(andThenThis)
+// Bên trong doThis nó có tham chiếu được gọi là callback, nó chỉ là một biến giữ tham chiếu đến chức năng này
+function andThenThis() {
+  console.log('and then this')
+}
+// Tên param thì gì cũng được
+function doThis(callback) {
+  console.log('this first')
+  
+  // the '()' is when you are telling your code to execute the function reference else it will just log the reference
+  // Gọi callback() là thực thi tham chiếu của hàm khác, nó sẽ chỉ log ra tham chiếu
+  callback()
+}
+
+
+
+//Promises
+
+function request(url) {
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    xhr.timeout = 2000;
+    xhr.onreadystatechange = function(e) {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(xhr.response)
+        } else {
+          reject(xhr.status)
+        }
+      }
+    }
+    xhr.ontimeout = function () {
+      reject('timeout')
+    }
+    xhr.open('get', url, true)
+    xhr.send();
+  })
+}
+
+const userGet = `https://api.github.com/search/users?page=1&q=diep&type=Users`
+const userRequest = request(userGet)
+
+userRequest
+  .then(handleUsersList)
+  .then(repoRequest)
+  .then(handleReposList)
+  .catch(handleErrors)
+  
+function handleUsersList(users) {
+  return JSON.parse(users).items
+}
+
+function repoRequest(users) {
+  return Promise.all(users.map(function(user) {
+    return request(user.repos_url)
+  }))
+}
+
+function handleReposList(repos) {
+  console.log('All users repos in an array', repos)
+}
+
+function handleErrors(error) {
+  console.error('Something went wrong ', error)
+}
+
+
+
+
+//Async/Await
+
+function afterTwoSeconds(value) {
+  return new Promise(resolve => {
+    setTimeout(() => { resolve(value) }, 2000);
+  });
+}
+
+async function sumTwentyAfterTwoSeconds(value) {
+  const remainder = afterTwoSeconds(20)
+  return value + await remainder
+}
+
+sumTwentyAfterTwoSeconds(10)
+  .then(result => console.log('after 2 seconds', result))
---
+++ object.js
@@ -0,0 +1,45 @@
+// Biến được khai báo bên trong hàm sẽ là biến cục bộ, và biến cục bộ thì có thể được truy cập bên trong hàm (không có khái niệm scope trong 1 khối lệnh).
+// Biến được khai báo bên ngoài hàm sẽ là biến toàn cục, và có thể được truy cập từ bất cứ đâu.
+
+function testScope()
+{
+    var local_var_1 = global_var_1 = "inside_function";
+    if(true){
+        var local_var_2 = "inside IF";
+    }
+    console.log("Test local_var_1 inside function: " + local_var_1);
+    console.log("Test local_var_2 inside function: " + local_var_2);
+}
+
+testScope();
+console.log("Test local_var_1 outside function: " + local_var_1);
+console.log("Test global_var_1 outside function: " + global_var_1);
+
+
+var num = 5;
+typeof(num);          //”number”
+ 
+//chạy lệnh lấy bình phương của số đó
+num.square();        // Uncaught TypeError: num.square is not a function
+//Mọi người thử trả lời câu hỏi tại sao lệnh lấy bình phương square() lại báo lỗi? Nếu bạn cho rằng kiểu dữ liệu number không có sẵn phương thức square() thì bạn đã trả lời đúng rùi đấy. Vậy câu hỏi đặt ra tiếp theo là: làm thế nào để ta khiến cho câu lệnh đó thực thi đúng như mong đợi? một trong số cách để giải quyết:
+
+Number.prototype.square = Number.prototype.square || function(){return this*this;};
+ 
+//Gọi thử lệnh để xem kết quả xem nào:
+num.square();               //25
+
+
+
+//Cách thứ nhất
+var printHocsinhName = Hocsinh.printName.bind(Hocsinh);
+printHocsinhName();                                      // John
+ 
+//Hoặc cách thứ 2
+var printHocsinhName = Hocsinh.printName;
+printHocsinhName.call(Hocsinh);                          // John
+ 
+//Xử lí Hocsinh.printName
+Hocsinh.printName.call(window);                           //Peter
+
+
+//https://viblo.asia/p/16-khai-niem-javascript-can-phai-nam-ro-phan-1-javascript-object-gGJ59jR9KX2
